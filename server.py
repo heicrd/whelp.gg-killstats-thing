ab6462ec49ca41587da407818afa853c142d69a9
heicrd <heicrd@cock.li>
2014-06-06 18:16:13 -0400

    searching

--- server.py
+++ server.py
@@ -35,10 +35,53 @@ class GenAsyncHandler(tornado.web.RequestHandler):
         sorted_systems = sorted(list(system_dic.items()), key=lambda p: -p[1])
         sorted_timezone = sorted(list(timezone_dic.items()), key=lambda p: -p[1])
         self.render("template.html",sd=sorted_systems,st=sorted_timezone)
+
+class SearchHandler(tornado.web.RequestHandler):
+    @tornado.gen.coroutine
+    def get(self):
+        http_client=tornado.httpclient.AsyncHTTPClient()
+        query = self.get_query_argument('query')
+        response = yield http_client.fetch("http://api.whelp.gg/search?q="+query)
+        j = tornado.escape.json_decode(response.body)
+        self.render('search.html',j=j)
+
+class ProfilePage(tornado.web.RequestHandler):
+    @tornado.gen.coroutine
+    def get(self, profile, profile_id):
+       # profile=self.profile
+        #profile_id=self.profile_id
+        http_client=tornado.httpclient.AsyncHTTPClient()
+        system_dic = {}
+        timezone_dic = {'EU':0,'US':0,'AU':0}
+        for p in range(1,6):
+            try:
+                response = yield http_client.fetch("http://api.whelp.gg/"+profile+"/"+profile_id+"?page="+str(p))
+                j = tornado.escape.json_decode(response.body)
+            except tornado.httpclient.HTTPError as err:
+                if err.code == 404:
+                    break
+                else:
+                    raise
+            for i in j['kills']:
+                system_dic.setdefault(i['system_name'],0)
+                system_dic[i['system_name']] += 1
+            for i in j['kills']:
+                c = int(i['kill_time'][11:13])
+                if c >= 0 and c < 8:
+                    timezone_dic['US']+=1
+                elif c >= 8 and c < 16:
+                    timezone_dic['AU']+=1
+                elif c >= 16:
+                    timezone_dic['EU']+=1
+        sorted_systems = sorted(list(system_dic.items()), key=lambda p: -p[1])
+        sorted_timezone = sorted(list(timezone_dic.items()), key=lambda p: -p[1])
+        self.render("template.html",sd=sorted_systems,st=sorted_timezone)
         
 
 application = tornado.web.Application([
     (r"/", GenAsyncHandler),
+    (r"/search",SearchHandler),
+    (r"/(character|corporation|alliance)/(\d+)",ProfilePage)
 ])
 
 if __name__ == "__main__":
