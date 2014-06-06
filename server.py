5057e0ebca5f2446f6f6255d609493775e28185c
heicrd <heicrd@cock.li>
2014-06-06 14:41:43 -0400

    descriptive variables

--- server.py
+++ server.py
@@ -10,8 +10,8 @@ class GenAsyncHandler(tornado.web.RequestHandler):
     @tornado.gen.coroutine
     def get(self):
         http_client = tornado.httpclient.AsyncHTTPClient()
-        ld = {}
-        td = {'EU':0,'US':0,'AU':0}
+        system_dic = {}
+        timezone_dic = {'EU':0,'US':0,'AU':0}
         for p in range(1,6):
             try:
                 response = yield http_client.fetch("http://api.whelp.gg/corporation/98182803?page="+str(p))
@@ -22,20 +22,19 @@ class GenAsyncHandler(tornado.web.RequestHandler):
                 else:
                     raise
             for i in j['kills']:
-                ld.setdefault(i['system_name'],0)
-                ld[i['system_name']] += 1
+                system_dic.setdefault(i['system_name'],0)
+                system_dic[i['system_name']] += 1
             for i in j['kills']:
-                c = int(str(i['kill_time'])[11:13])
+                c = int(i['kill_time'][11:13])
                 if c >= 0 and c < 8:
-                    td['US']+=1
+                    timezone_dic['US']+=1
                 elif c >= 8 and c < 16:
-                    td['AU']+=1
+                    timezone_dic['AU']+=1
                 elif c >= 16:
-                    td['EU']+=1
-        sd = sorted(list(ld.items()), key=lambda p: -p[1])
-        st = sorted(list(td.items()), key=lambda p: -p[1])
-        ws="Kill Systems\n" + str(sd) + "Kill TZ\n" + str(st)
-        self.render("template.html",sd=sd,st=st)
+                    timezone_dic['EU']+=1
+        sorted_systems = sorted(list(system_dic.items()), key=lambda p: -p[1])
+        sorted_timezone = sorted(list(timezone_dic.items()), key=lambda p: -p[1])
+        self.render("template.html",sd=sorted_systems,st=sorted_timezone)
         
 
 application = tornado.web.Application([
