07ac0e77b82c5cdf9d26438446d1235cf31515eb
heicrd <heicrd@cock.li>
2014-06-06 18:42:59 -0400

    added page titles, cleared homepage

--- server.py
+++ server.py
@@ -6,35 +6,10 @@ import tornado.httpclient
 import tornado.escape
 import json
 
-class GenAsyncHandler(tornado.web.RequestHandler):
+class Homepage(tornado.web.RequestHandler):
     @tornado.gen.coroutine
     def get(self):
-        http_client = tornado.httpclient.AsyncHTTPClient()
-        system_dic = {}
-        timezone_dic = {'EU':0,'US':0,'AU':0}
-        for p in range(1,6):
-            try:
-                response = yield http_client.fetch("http://api.whelp.gg/corporation/98182803?page="+str(p))
-                j = tornado.escape.json_decode(response.body)
-            except tornado.httpclient.HTTPError as err:
-                if err.code == 404:
-                    break
-                else:
-                    raise
-            for i in j['kills']:
-                system_dic.setdefault(i['system_name'],0)
-                system_dic[i['system_name']] += 1
-            for i in j['kills']:
-                c = int(i['kill_time'][11:13])
-                if c >= 0 and c < 8:
-                    timezone_dic['US']+=1
-                elif c >= 8 and c < 16:
-                    timezone_dic['AU']+=1
-                elif c >= 16:
-                    timezone_dic['EU']+=1
-        sorted_systems = sorted(list(system_dic.items()), key=lambda p: -p[1])
-        sorted_timezone = sorted(list(timezone_dic.items()), key=lambda p: -p[1])
-        self.render("template.html",sd=sorted_systems,st=sorted_timezone)
+        self.render("homepage.html")
 
 class SearchHandler(tornado.web.RequestHandler):
     @tornado.gen.coroutine
@@ -43,7 +18,7 @@ class SearchHandler(tornado.web.RequestHandler):
         query = self.get_query_argument('query')
         response = yield http_client.fetch("http://api.whelp.gg/search?q="+tornado.escape.url_escape(query, plus=False))
         j = tornado.escape.json_decode(response.body)
-        self.render('search.html',j=j)
+        self.render('search.html',j=j,query=query)
 
 class ProfilePage(tornado.web.RequestHandler):
     @tornado.gen.coroutine
@@ -73,11 +48,13 @@ class ProfilePage(tornado.web.RequestHandler):
                     timezone_dic['EU']+=1
         sorted_systems = sorted(list(system_dic.items()), key=lambda p: -p[1])
         sorted_timezone = sorted(list(timezone_dic.items()), key=lambda p: -p[1])
-        self.render("template.html",sd=sorted_systems,st=sorted_timezone)
+        j = tornado.escape.json_decode((yield http_client.fetch("http://api.whelp.gg/"+profile_type+"/"+profile_id)).body)
+        name = j['stats'][profile_type+'_name']
+        self.render("profile_page.html",sd=sorted_systems,st=sorted_timezone,name=name)
         
 
 application = tornado.web.Application([
-    (r"/", GenAsyncHandler),
+    (r"/", Homepage),
     (r"/search",SearchHandler),
     (r"/(character|corporation|alliance)/(\d+)",ProfilePage)
 ])
